5c8206580c51f9386b20334ccfe484c00be1e001
Rozi85 <rozifatkhur85@gmail.com>
2023-11-22 18:17:59 +0700

    kuku

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] private ParticleSystem hit;
+    Transform player;
+    Vector3 hitDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        hitDir = player.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        hit.transform.localScale = hitDir;
     }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Attack")
         {
+            hitDir = player.transform.localScale;
+            Debug.Log("Hit");
             hit.Play();
         }
+        
     }
 }
